96d366c02664d2f42cdf6396be94978a87283d37
areebaqamar021 <areebaqamar021@gmail.com>
2023-11-07 12:53:42 +0500

    changings

--- tailwindProject/tailwind.config.js
+++ tailwindProject/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.html'],
+  content: ['./*.html'],
   theme: {
     screens: {
       sm: '480px',
@@ -10,6 +10,16 @@ module.exports = {
     },
     extend: {
       colors: {
+        primaryColor: '#010a50',
+        primaryColorLight: '#010d78',
+        secondaryColor: '#FFCC00',
+        paragraphColor: '#c0c0c0',
+        whiteColor: '#fff',
+        blackColor: '#000',
+        greenColor: '#007936',
+        redColor: '#cc3433',
+        darkColor: '#000',
+        darkColorLight: '#171717',
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
@@ -23,4 +33,3 @@ module.exports = {
   },
   plugins: [],
 }
-
